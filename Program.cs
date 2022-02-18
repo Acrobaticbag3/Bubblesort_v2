e3b264663f4657f35b7e676530ac516c9713dcd0
Acrobaticbag3 <kevintom214@gmail.com>
2022-02-18 14:15:44 +0100

    oyoy

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace Bubbelsort_Algotithem {
     public class HelloWorld {
     
         public static void Main(string[] args) {
-            int[] numberArray = new int[8000]; // Array with 1000 ints
+            int[] numberArray = new int[1000]; // Array with 1000 ints
             Random intRng = new Random(); // Random number generator
         
             bool flag = true;  
             int temp;  
             int arrayLength = numberArray.Length; // The length of the array.
         
-            for(int i = 0; i < 8000 ; i++) { // Assigns random values to the ints
+            for(int i = 0; i <1000 ; i++) { // Assigns random values to the ints
                 numberArray[i] = intRng.Next(1, 1001);
                 Console.Write(numberArray[i] + " ");
             }
